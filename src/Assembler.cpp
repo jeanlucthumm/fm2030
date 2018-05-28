0f396d17c398f43f29b86a63aaef866d401d0b09
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-05-28 15:40:58 -0700

    Add format and composite flags in table

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -7,76 +7,122 @@
 using namespace std;
 
 const unordered_map<string, OpEntry> Assembler::opTable = { // NOLINT(cert-err58-cpp)
-    {"add", {
-                2,
-                0x00,
-            }},
-    {"cmp", {
-                2,
-                0x01,
-            }},
-    {"clr", {
-                1,
-                0x02,
-            }},
-    {"shl", {
-                2,
-                0x03,
-            }},
+    {"add",   {
+                  2,
+                  0x00,
+                  R,
+                  false,
+              }},
+    {"cmp",   {
+                  2,
+                  0x01,
+                  R,
+                  false,
+              }},
+    {"clr",   {
+                  1,
+                  0x02,
+                  R,
+                  false,
+              }},
+    {"shl",   {
+                  2,
+                  0x03,
+                  R,
+                  false,
+              }},
     {"lfsrs", {
-                1,
-                0x04,
-            }},
+                  1,
+                  0x04,
+                  R,
+                  false,
+              }},
     {"lfsrn", {
-                2,
-                0x05,
-            }},
+                  2,
+                  0x05,
+                  R,
+                  false,
+              }},
     {"lfsrp", {
-                2,
-                0x06,
-            }},
-    {"ld", {
-                2,
-                0x07,
-            }},
-    {"st", {
-                2,
-                0x08,
-            }},
+                  2,
+                  0x06,
+                  R,
+                  false,
+              }},
+    {"ld",    {
+                  2,
+                  0x07,
+                  R,
+                  false,
+              }},
+    {"st",    {
+                  2,
+                  0x08,
+                  R,
+                  false,
+              }},
     {"moved", {
-                2,
-                0x09,
-            }},
+                  2,
+                  0x09,
+                  R,
+                  false,
+              }},
     {"mover", {
-                2,
-                0x0A,
-            }},
-    {"inc", {
-                1,
-                0x0B,
-            }},
-    {"xor", {
-                2,
-                0x0C,
-            }},
-    {"be", {
-                1,
-                0x0D,
-            }},
-    {"bne", {
-                1,
-                0x0E,
-            }},
-    {"jump", {
-                1,
-                0x0F,
-            }},
-    {"set", {
-                 2,
-                 0x00,
-             }},
-    {"mov", {
-                 2,
-                 0x00,
-             }},
+                  2,
+                  0x0A,
+                  R,
+                  false,
+              }},
+    {"inc",   {
+                  1,
+                  0x0B,
+                  R,
+                  false,
+              }},
+    {"xor",   {
+                  2,
+                  0x0C,
+                  R,
+                  false,
+              }},
+    {"be",    {
+                  1,
+                  0x0D,
+                  B,
+                  false,
+              }},
+    {"bne",   {
+                  1,
+                  0x0E,
+                  B,
+                  false,
+              }},
+    {"jump",  {
+                  1,
+                  0x0F,
+                  B,
+                  false,
+              }},
+    {"set",   {
+                  2,
+                  0x00,
+                  R,
+                  true,
+              }},
+    {"mov",   {
+                  2,
+                  0x00,
+                  R,
+                  true,
+              }},
 };
+
+Assembler::Assembler(Scanner &scanner) : scanner{scanner}, counter{0} {}
+
+/// \throw Any error from Scanner::nextOp()
+void Assembler::assemble(const std::ofstream &out) {
+    scanner.reset();
+    while (!scanner.eof()) {
+        vector<string> instr = scanner.nextOp();
+    }
+}
--- src/Assembler.hpp
+++ src/Assembler.hpp
@@ -11,21 +11,28 @@
 #include <fstream>
 #include "Scanner.hpp"
 
+enum InstrFormat {
+    R, B
+};
+
 struct OpEntry{
     int numOperands;
     unsigned char opCode;
+    InstrFormat format;
+    bool composite;
 };
 
 class Assembler {
 public:
     const static std::unordered_map<std::string, OpEntry> opTable;
 
-    Assembler(Scanner &scanner);
+    explicit Assembler(Scanner &scanner);
 
     void assemble(const std::ofstream &out);
 
 private:
     Scanner &scanner;
+    int counter;
 };
 
 
