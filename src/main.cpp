e40a0eea9948cf85d12a1c72f81ac4d33e2a41ba
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-05-28 16:51:35 -0700

    Remove debugging from main

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,5 @@
 using namespace std;
 
 int main() {
-    ifstream in{"assembly/program1.s"};
-    Scanner scanner{in};
-
-    instr_t instr = Assembler::rFormat(0x0C, 0x00, 0x02, 0x00);
-
-    cout << hex << uppercase << instr << endl;
-
     return EXIT_SUCCESS;
 }
