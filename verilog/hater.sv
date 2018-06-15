ffd0bab12b6695d30c7cd8b695e34d0effaddd9a
msparrow <msparrow@ucsd.edu>
2018-06-15 08:27:41 -0700

    Update hater.sv

--- verilog/hater.sv
+++ verilog/hater.sv
@@ -1,15 +1,15 @@
 module  Hater(
-input[0:7]  PC       , // Current PC counter value 
-input[0:7]  PC_End     , //  PC counter value upon program completion
-//input[1:0]  wire  ctrl,
+	input[0:7]  PC_Curr    , // Current PC counter value 
+	input[0:7]  PC_End     , //  PC counter value upon program completion
+	//input[1:0]  wire  ctrl,
 
-output logic halt      //Halt signal
+	output logic halt      //Halt signal
 );
 
 always_comb
 begin
 
-	if(PC == PC_End) begin 
+	if(PC_Curr == PC_End) begin 
 		halt = 1;
 		
 	end else begin
