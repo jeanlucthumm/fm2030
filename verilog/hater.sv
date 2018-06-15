4dbcc1c38acf08a1c7ef35e105fc7e91ba44a885
msparrow <msparrow@ucsd.edu>
2018-06-15 08:33:51 -0700

    Updated halter to use internal registers

--- verilog/hater.sv
+++ verilog/hater.sv
@@ -1,15 +1,23 @@
 module  Hater(
-	input[0:7]  PC_Curr    , // Current PC counter value 
-	input[0:7]  PC_End     , //  PC counter value upon program completion
-	//input[1:0]  wire  ctrl,
+input[0:7]  PC_Curr       , // Current PC counter value 
+input[0:7]  PC_End     , //  PC counter value upon program completion
+//input[1:0]  wire  ctrl,
 
-	output logic halt      //Halt signal
+output logic halt      //Halt signal
 );
+
+reg set;
+reg[7:0] endState; 
+
+initial 
+begin
+	endState = PC_End; 
+end
 
 always_comb
 begin
 
-	if(PC_Curr == PC_End) begin 
+	if(PC_Curr == endState) begin 
 		halt = 1;
 		
 	end else begin
